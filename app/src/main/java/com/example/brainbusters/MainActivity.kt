9da21644799ccb70d152e9b4f1f2d4a78a08e8b6
Samuele Ferri <samuele1301@gmail.com>
2024-05-20 17:48:27 +0200

    add navigationBar and components.kt, error in Text

--- app/src/main/java/com/example/brainbusters/MainActivity.kt
+++ app/src/main/java/com/example/brainbusters/MainActivity.kt
@@ -6,11 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.brainbusters.ui.AppBar
+import com.example.brainbusters.ui.Categories
+import com.example.brainbusters.ui.Quiz
 import com.example.brainbusters.ui.theme.BrainBustersTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +29,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BrainBustersTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+
+                    Scaffold(
+                        topBar = { AppBar(navController) },
+                    ) { contentPadding ->
+                        NavGraph(navController, modifier = Modifier.padding(contentPadding))
+                    }
                 }
             }
         }
@@ -34,7 +50,9 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.primary,
+        fontSize = 18.sp
     )
 }
 
@@ -43,5 +61,35 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     BrainBustersTheme {
         Greeting("Android")
+    }
+}
+
+sealed class NavigationRoute(
+    val route: String
+) {
+    data object Categories : NavigationRoute("categories")
+    data object Quiz : NavigationRoute("quiz")
+}
+
+@Composable
+fun NavGraph(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = NavigationRoute.Categories.route,
+        modifier
+    ) {
+        with(NavigationRoute.Categories) {
+            composable(route) {
+                Categories(navController)
+            }
+        }
+        with(NavigationRoute.Quiz) {
+            composable(route) {
+                Quiz(navController)
+            }
+        }
     }
 }
