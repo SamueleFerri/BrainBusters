7bca6804a1f380c586d2eae83f9201bef2cd80a1
Samuele Ferri <samuele1301@gmail.com>
2024-05-21 10:03:31 +0200

    error resolve, libraries outdated

--- app/src/main/java/com/example/brainbusters/ui/Components.kt
+++ app/src/main/java/com/example/brainbusters/ui/Components.kt
@@ -1,13 +1,7 @@
 package com.example.brainbusters.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material3.Button
