99fbaae85beb06b3de962008189c7096486c243b
LucaFerar <luca.ferartofan@studio.unibo.it>
2024-06-05 15:25:15 +0200

    fixed backend problem

--- app/src/main/java/com/example/brainbusters/BrainBusterApplication.kt
+++ app/src/main/java/com/example/brainbusters/BrainBusterApplication.kt
@@ -1,4 +1,18 @@
 package com.example.brainbusters
 
-class BrainBusterApplication {
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class BrainBusterApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@BrainBusterApplication)
+            modules(dataModule)
+        }
+    }
 }
