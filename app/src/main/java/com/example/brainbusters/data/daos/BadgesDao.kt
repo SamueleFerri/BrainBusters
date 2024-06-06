cdb5fa2641b245313cc9595b3f7f2995d52f2981
LucaFerar <luca.ferartofan@studio.unibo.it>
2024-06-06 12:03:54 +0200

    added backend badges

--- app/src/main/java/com/example/brainbusters/data/daos/BadgesDao.kt
+++ app/src/main/java/com/example/brainbusters/data/daos/BadgesDao.kt
@@ -1,4 +1,25 @@
 package com.example.brainbusters.data.daos
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.brainbusters.data.entities.Badge
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface BadgesDao {
+
+    // Insert Badge
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(badge: Badge)
+
+
+    // Get All Badge
+    @Query("SELECT * FROM badges")
+    fun getAllbadges(): Flow<List<Badge>>
+
+    // Get Badge by id
+    @Query("SELECT * FROM badges WHERE badge_id = :badgeId")
+    fun getBadgeById(badgeId: Int): Flow<Badge>
 }
--- app/src/main/java/com/example/brainbusters/data/repositories/BadgeRepository.kt
+++ app/src/main/java/com/example/brainbusters/data/repositories/BadgeRepository.kt
@@ -1,4 +1,18 @@
 package com.example.brainbusters.data.repositories
 
-class BadgeRepository {
+import com.example.brainbusters.data.daos.BadgesDao
+import com.example.brainbusters.data.entities.Badge
+import kotlinx.coroutines.flow.Flow
+
+class BadgeRepository(private val badgesDAO: BadgesDao) {
+
+    val badges: Flow<List<Badge>> = badgesDAO.getAllbadges()
+
+    suspend fun insertNewBadge(badge: Badge) {
+        badgesDAO.insert(badge)
+    }
+
+    fun getAllBages() {
+        badgesDAO.getAllbadges()
+    }
 }
